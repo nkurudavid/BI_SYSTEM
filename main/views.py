cf25c452cdfa75f14162a987301f0d179d421adc
nkurudavid <nkurudavid44@gmail.com>
2023-07-27 23:23:05 +0200

    new configuration

--- main/views.py
+++ main/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 
 
 # Create your views here.
+def handle_not_found(request, exception):
+    return render(request, 'page_404/404.html')
+
 
 def home(request):
     return render(request, 'main/home.html');
